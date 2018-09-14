2744891c98bae5eef452f99031eced08bd4a1e1a
liyong <liyong@163.com>
2018-09-14 11:14:58 +0800

    update private

--- lib/src/main/java/com/allen/android/lib/RxActivityResult.java
+++ lib/src/main/java/com/allen/android/lib/RxActivityResult.java
@@ -14,7 +14,7 @@ import io.reactivex.Observable;
  */
 public class RxActivityResult {
 
-  public static final String TAG = "RxActivityResult";
+  private static final String TAG = "RxActivityResult";
   private Lazy<RxActivityResultFragment> mRxActivityResultFragment;
 
   public RxActivityResult(@NonNull FragmentActivity activity) {
